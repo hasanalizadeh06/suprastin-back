3495cb849330a807efa64dea152594ac237acb70
HasanAlizadeh <texnovideo181@gmail.com>
2025-09-22 14:50:53 +0400

    deployment

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:3001',
+      'http://localhost:3000',
       'https://suprastin.vercel.app',
     ],
     credentials: true,
